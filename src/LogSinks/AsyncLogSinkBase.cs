8770f943bc07428d142c98accb75b59e51e939f0
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-05-16 20:34:03 +0200

    feat: refactor constructors in AsyncLogSinkBase and LogSinkBase to use required DefaultLogMessageWriter property

--- src/LogSinks/AsyncLogSinkBase.cs
+++ src/LogSinks/AsyncLogSinkBase.cs
@@ -23,12 +23,11 @@ public abstract class AsyncLogSinkBase<TLogSinkBase> : IAsyncLogSink, IAsyncDisp
     /// <summary>
     /// Initializes a new instance of the <see cref="AsyncLogSinkBase{TLogSinkBase}"/> class with the specified default log message writer.
     /// </summary>
-    /// <param name="defaultLogMessageWriter">The default log message writer to use for payload types that do not have a specific log message writer registered.</param>
-    protected AsyncLogSinkBase(IAsyncLogMessageWriter<object> defaultLogMessageWriter)
+    protected AsyncLogSinkBase()
     {
         logMessageWriterPipeline = new()
         {
-            DefaultLogMessageWriter = defaultLogMessageWriter
+            DefaultLogMessageWriter = DefaultLogMessageWriter
         };
 
         logMessageWriterPipeline.Container.RegisterInstance<IAsyncLogSink>(this);
@@ -37,6 +36,11 @@ public abstract class AsyncLogSinkBase<TLogSinkBase> : IAsyncLogSink, IAsyncDisp
     // ┌─────────────────────────────────────────────────────────────────────────────┐
     // │ Public Properties                                                           │
     // └─────────────────────────────────────────────────────────────────────────────┘
+
+    /// <summary>
+    /// Gets the default <see cref="IAsyncLogMessageWriter{TPayload}"/> to use for payload types that do not have a specific log message writer registered.
+    /// </summary>
+    public required IAsyncLogMessageWriter<object> DefaultLogMessageWriter { get; init; }
 
     /// <summary>
     /// Gets the <see cref="IContainer"/> associated with this log sink.
